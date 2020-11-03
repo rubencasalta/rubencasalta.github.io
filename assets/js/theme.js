b935d7bdca563b001175b76f7408dffae5154f69
Rubén Casalta <ruben.casalta@gmail.com>
2020-11-03 12:11:10 +0100

    Iniciando Vue.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,10 @@
 	var app = new Vue({
 		el: '#app',
 		data: {
-			title: 'Rubén Casalta con Vue.js',
+			title: 'Rubén Casalta',
+			lead: 'Aunque  todo mis proyectos para clientes los tengo como privados puedes ver mi tema básico para Wordpress con el que desarrollo todo mi trabajo profesional.',
+			email: 'ruben.casalta@gmail.com',
+			phone: '644 496 836',
 			fruits: [
 				{name:'Manzana',amount:3},
 				{name:'Pera',amount:0},
