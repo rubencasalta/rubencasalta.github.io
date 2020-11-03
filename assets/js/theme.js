157f2e2b4a713cc38128fc7614f120a2153bca97
Rubén Casalta <ruben.casalta@gmail.com>
2020-11-03 11:43:39 +0100

    Iniciando Vue.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,6 +6,15 @@
 				{name:'Manzana',amount:3},
 				{name:'Pera',amount:0},
 				{name:'Platano',amount:45},
-			]
+			],
+			newFruit: ''
+		},
+		methods: {
+			addFruit () {
+				this.fruits.push({
+					name: this.newFruit,
+					amount: 1
+				})
+			}
 		}
 	});
