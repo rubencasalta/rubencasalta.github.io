8eaeb17be8c5ac39f2b1cc124e840c173ee24fc2
Rubén Casalta <ruben.casalta@gmail.com>
2020-11-05 22:42:54 +0100

    Ejercicios de Vue.js v2

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,5 @@
-	var app = new Vue({
+
+	var appUno = new Vue({
 		el: '#app',
 		data: {
 			title: 'Rubén Casalta',
@@ -18,6 +19,52 @@
 					name: this.newFruit,
 					amount: 1
 				})
+			}
+		}
+	});
+
+	var apDos = new Vue({
+		el: '#app-2',
+		data: {
+			bg: 'bg-warning',
+			color: false
+		},
+		methods: {
+
+		}
+	});
+
+	var apTres = new Vue({
+		el: '#app-3',
+		data: {
+			msg: "Hola soy Musca",
+			count: 0
+		},
+		computed: {
+			msgReverse() {
+				return this.msg.split('').reverse().join('');
+			},
+			color() {
+				return {
+					'bg-success' : this.count <= 20,
+					'bg-primary' : this.count >= 40,
+					'bg-warning' : this.count >= 60,
+					'bg-danger' : this.count > 80
+				}
+			}
+		},
+		methods: {
+			addCount() {
+				this.count = this.count + 10;
+				if(this.count > 100) {
+					this.count = 100;
+				}
+			},
+			subCount() {
+				this.count = this.count - 10;
+				if(this.count < 0) {
+					this.count = 0;
+				}
 			}
 		}
 	});
